f22fcd4f418428e92e548acf42a10b1a5deec3b9
jaricappp <50119582+jaricappp@users.noreply.github.com>
2019-05-08 17:41:30 -0400

    Create flip.js

---
+++ commands/flip.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+exports.run = async (client, message, args) => {
+let outcomes = ["Heads", "Tails"];
+let outcomesIndex = Math.round(Math.random() * outcomes.length);
+const e = await message.channel.send("*A holographic coin materializes before you, and falls.*");
+setTimeout(() => {
+    e.edit(`*It lands on ${outcomes[outcomesIndex]}.*`);
+}, 1500);
+}
+
+module.exports.help = {
+  name: "flip"
+}
