0294cb226f331dc114a84c4c9c51ceb3c624c4a6
jaricappp <50119582+jaricappp@users.noreply.github.com>
2019-05-08 18:52:43 -0400

    Update rich.js

--- commands/rich.js
+++ commands/rich.js
@@ -2,10 +2,11 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
     if (message.author.id !== '320255429080776707') return message.reply("I'm not saying that.");
     const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{}); 
+
     let embed = new Discord.RichEmbed ()
         .setColor('#0964f7')
         .setDescription(sayMessage)
+        message.delete().catch(O_o=>{}); 
         message.channel.send(embed);
 }
 
