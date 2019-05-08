ebc99a9a55fa6dcb874a3559c1f56302b04109b3
jaricappp <50119582+jaricappp@users.noreply.github.com>
2019-05-08 19:05:05 -0400

    Update letsHaveShrex.js

--- commands/letsHaveShrex.js
+++ commands/letsHaveShrex.js
@@ -1,11 +1,10 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
-  var amember = message.mentions.users.first();
-    if(!amember){
   let embed = new Discord.RichEmbed()
   .setImage("https://media.discordapp.net/attachments/560252415324979211/575819470023688203/t29pa09kghf11.png?width=290&height=300")
   .setColor('#275BF0')
     message.channel.send(embed)
+};
     module.exports.help = {
   name: "letsHaveShrex"
 }
