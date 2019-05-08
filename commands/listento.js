77276cd446cfb01623acb4bdd003d56715b66787
jaricappp <50119582+jaricappp@users.noreply.github.com>
2019-05-08 17:56:09 -0400

    Update listento.js

--- commands/listento.js
+++ commands/listento.js
@@ -1,7 +1,6 @@
 exports.run = (client, message, args) => {
         const watchmessage = args.join(" ");
-    if(!message.member.roles.some(r=>["Commanding Officer", "HIGH COMMAND"].includes(r.name)) )
-    return message.reply("Nice try.");
+    if (message.author.id !== '320255429080776707') return message.reply("Nice try.");
     
     client.user.setActivity(watchmessage, {type: 2});
     
