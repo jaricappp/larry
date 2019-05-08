589f2d026d6a9a1313f40def994d584197efab1c
jaricappp <50119582+jaricappp@users.noreply.github.com>
2019-05-08 17:57:13 -0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
-if (message.author.id !== '178242199924899840') return message.reply("I'm not saying that.");
+    if (message.author.id !== '320255429080776707') return message.reply("I'm not saying that.");
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
     message.channel.send(sayMessage);
