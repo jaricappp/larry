42efab908b3ae66ff46e9de1110bae6ef5a7c2d0
jaricappp <50119582+jaricappp@users.noreply.github.com>
2019-05-08 17:49:46 -0400

    Create rich.js

---
+++ commands/rich.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+exports.run = (client, message, args) => {
+if(!message.member.roles.some(r=>["Commanding Officer"].includes(r.name)) )
+      return message.reply("I'm not saying that.");
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{}); 
+    let embed = new Discord.RichEmbed ()
+        .setColor('#0964f7')
+        .setDescription(sayMessage)
+        message.channel.send(embed);
+}
+
+module.exports.help = {
+  name: "rich"
+}
